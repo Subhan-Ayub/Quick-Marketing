fd547dd6fce203fc2f91d407d79229fd75761e0f
ahsan.awan <ahsan.awan@sofodynamix.com>
2024-03-24 17:30:51 +0500

    ahsan today

--- lib/src/controllers/planController.dart
+++ lib/src/controllers/planController.dart
@@ -5,35 +5,111 @@ class PlanController extends GetxController {
   RxBool social = false.obs;
   RxBool photography = false.obs;
 
-  List cardData=[
-    {
-    'offer':'FaceBook 8 Post',
-    'tick':true
-    },  {
-    'offer':'Instagram 8 Post',
-    'tick':true
-    },  {
-    'offer':'Content Creation',
-    'tick':true
-    },  {
-    'offer':'Campaing Execution',
-    'tick':true
-    },  {
-    'offer':'Graphic Desinging',
-    'tick':true
-    },  {
-    'offer':'Social Media Insight Report',
-    'tick':true
-    },  {
-    'offer':'Tweets',
-    'tick':false
-    },  {
-    'offer':' Competitior Analysis Report',
-    'tick':false
-    },  {
-    'offer':'Competitior Analysis Report',
-    'tick':false
-    },
-    ];
 
+
+  List webCardDataBasic = [
+    {'offer': 'FaceBook 8 Post 1111111111111111111', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List webCardDataStandard = [
+    {'offer': 'FaceBook 8 Post 222222222222222', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List webCardDataPlatinium = [
+    {'offer': 'FaceBook 8 Post  33333333333333', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List socialCardDataBasic = [
+    {'offer': 'FaceBook 8 Post 2111111111', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List socialCardDataStandard = [
+    {'offer': 'FaceBook 8 Post 222222222222', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+  List socialCardDataPlatinium = [
+    {'offer': 'FaceBook 8 Post 23333333333333', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List productCardDataBasic = [
+    {'offer': 'FaceBook 8 Post  311111111111111', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+
+  List productCardDataStandard = [
+    {'offer': 'FaceBook 8 Post 322222222222222222', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
+  List productCardDataPlatinium = [
+    {'offer': 'FaceBook 8 Post 33333333333333333333333', 'tick': true},
+    {'offer': 'Instagram 8 Post', 'tick': true},
+    {'offer': 'Content Creation', 'tick': true},
+    {'offer': 'Campaing Execution', 'tick': true},
+    {'offer': 'Graphic Desinging', 'tick': true},
+    {'offer': 'Social Media Insight Report', 'tick': true},
+    {'offer': 'Tweets', 'tick': false},
+    {'offer': ' Competitior Analysis Report', 'tick': false},
+    {'offer': 'Competitior Analysis Report', 'tick': false},
+  ];
 }
